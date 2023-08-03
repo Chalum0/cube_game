ebd4716d029d97a9cfa777465148c1591a1252c7
Snappyink2 <raphaelzanatta24@proton.me>
2023-08-03 13:34:54 +0200

    added base files with base code

---
+++ main.py
@@ -0,0 +1,25 @@
+import settings
+import pygame
+import random
+import time
+import game
+
+pygame.init()
+pygame.font.init()
+pygame.display.set_caption("")
+screen = pygame.display.set_mode((1080, 720))
+clock = pygame.time.Clock()
+max_fps = 60
+screen_x, screen_y = screen.get_size()
+
+playing = True
+while playing:
+
+
+    pygame.display.flip()
+    clock.tick(max_fps)
+    # events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            time.sleep(0.2)
+            playing = False
