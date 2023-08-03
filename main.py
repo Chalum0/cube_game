0f57d22e7952857db16fb5f8a4a689741831f973
Snappyink2 <raphaelzanatta24@proton.me>
2023-08-03 14:04:17 +0200

    ez ?

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import settings
+import setting
 import pygame
 import random
 import time
 import game
 import math
+
 
 pygame.init()
 pygame.font.init()
@@ -15,9 +16,27 @@ screen_x, screen_y = screen.get_size()
 
 game = game.Game()
 
+camX = 0
+camY = 0
+
+player_pos = (0, 0, 0)
+fov = 400
+
 playing = True
 while playing:
 
+    view_matrix = (math.sin(camX)*math.sin(camY), math.sin(camX)*math.cos(camY), math.cos(camX)*math.cos(camY), math.cos(camX)*math.sin(camY))
+
+    point = (0, 0, -10)  # coordonnées dans le world space
+    point = (point[0]-player_pos[0], point[1]-player_pos[1], point[2]-player_pos[2])
+    transformed_point = (point[0] * math.cos(camY) + point[2] * math.sin(camY), point[0] * view_matrix[0] + point[1] * math.cos(camX) - point[2] * view_matrix[1], point[1] * math.sin(camX) + point[2] * view_matrix[2] - point[0] * view_matrix[3])
+    print(transformed_point)
+    if transformed_point[2] > 0:
+        point = (transformed_point[0] * fov / transformed_point[2], transformed_point[1] * fov / transformed_point[2]) #coordonnées dans le screen space, soit point = (x*fov/z, y*fov/z)
+    else:
+        pass #le point est derrière la caméra
+
+    pygame.draw.rect(screen, (255, 0, 0), pygame.rect.Rect(point[0]-10, point[1]-10, 20, 20))
 
     pygame.display.flip()
     clock.tick(max_fps)
