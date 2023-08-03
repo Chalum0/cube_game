d1f830c2bd5a14278ae58c479bdfe264ed2a5458
maDU59_ <matthias.ducourant@gmail.com>
2023-08-03 21:05:28 +0200

    Update game.py

--- game.py
+++ game.py
@@ -8,16 +8,18 @@ class Game:
 
     def display_rect(self, points: tuple, view_matrix: tuple, screen_x: int, screen_y: int, screen: pygame.surface.Surface):
         ps = []
+        vspoints = []
         for point in points:
             point = (point[0]-self.player.pos[0], point[1]-self.player.pos[1], point[2]-self.player.pos[2])
             transformed_point = (point[0] * math.cos(self.player.camY) + point[2] * math.sin(self.player.camY),
                                  point[0]*view_matrix[0] + point[1] * math.cos(self.player.camX) - point[2] * view_matrix[1],
                                  point[1] * math.sin(self.player.camX) + point[2] * view_matrix[2] - point[0] * view_matrix[3])
+            vspoints.append(transformed_poin)
             if point[2]>0:
                 point = (transformed_point[0] * self.player.fov / transformed_point[2], transformed_point[1] * self.player.fov / transformed_point[2]) #coordonnées dans le screen space, soit point = (x*fov/z, y*fov/z)
                 pygame.draw.rect(screen, (255, 0, 0), pygame.rect.Rect(point[0]-10 + screen_x/2, point[1]-10 + screen_y/2, 20, 20))
                 ps.append((point[0] + screen_x/2, point[1] + screen_y/2, 20, 20))
-        return ps
+        return ps, vspoints
 
     def view_matrix(self):
         return (math.sin(self.player.camX)*math.sin(self.player.camY),
