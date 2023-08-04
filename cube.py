82d44028463d80b9b86f292e4b92ef6a6c5937ba
maDU59_ <matthias.ducourant@gmail.com>
2023-08-04 14:55:51 +0200

    Update cube.py

--- cube.py
+++ cube.py
@@ -5,7 +5,7 @@ import math
 
 
 class Block:
-    def __init__(self, coord, player_coords, map_, matrix_coords, vm, camX, camY):
+    def __init__(self, coord, player_coords, map_, matrix_coords, vm):
         self.points = ((coord[0] + half_block, coord[1] + half_block, coord[2] - half_block),
                 (coord[0] + half_block, coord[1] - half_block, coord[2] - half_block),
                 (coord[0] - half_block, coord[1] - half_block, coord[2] - half_block),
@@ -22,9 +22,7 @@ class Block:
         self.player_distance = math.dist(self.coord, player_coords)
         self.sphere_size = math.sqrt(half_block**2 *3)
         self.vm = vm
-        self.camX = camX
-        self.camY = camY
-        self.update_render(player_coords, map_, vm, camX, camY)
+        self.update_render(player_coords, map_, vm)
 
     def __lt__(self, other):
         return self.player_distance < other.player_distance
@@ -32,7 +30,7 @@ class Block:
     def update_distance(self, player_pos):
         self.player_distance = math.dist(self.coord, player_pos)
 
-    def update_render(self, player_pos, map_, vm, camX, camY):
+    def update_render(self, player_pos, map_, vm):
         self.faces = []
         point = (self.coord[0]-player_pos[0], self.coord[1]-player_pos[1], self.coord[2]-player_pos[2])            
         if not(point[1] * vm[4] + point[2] * vm[2] - point[0] * vm[3] < -self.sphere_size or self.type == 0):
